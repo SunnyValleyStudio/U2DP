4c486673b7450a32e3c881c824e0a0a56e677496
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2021-04-22 09:32:54 +0200

    Lecture 24 AgentAnimation script

--- Udemy 2d Platformer/Assets/_Scripts/Agent.cs
+++ Udemy 2d Platformer/Assets/_Scripts/Agent.cs
@@ -6,11 +6,13 @@ public class Agent : MonoBehaviour
 {
     public Rigidbody2D rb2d;
     public PlayerInput playerInput;
+    public AgentAnimation animationManager;
 
     private void Awake()
     {
         playerInput = GetComponentInParent<PlayerInput>();
         rb2d = GetComponent<Rigidbody2D>();
+        animationManager = GetComponentInChildren<AgentAnimation>();
     }
 
     private void Start()
@@ -22,10 +24,18 @@ public class Agent : MonoBehaviour
     {
         if (Mathf.Abs(input.x) > 0)
         {
+            if (Mathf.Abs(rb2d.velocity.x) < 0.01f)
+            {
+                animationManager.PlayAnimation(AnimationType.run);
+            }
             rb2d.velocity = new Vector2(input.x, 0) * 5;
         }
         else
         {
+            if (Mathf.Abs(rb2d.velocity.x) > 0)
+            {
+                animationManager.PlayAnimation(AnimationType.idle);
+            }
             rb2d.velocity = Vector2.zero;
         }
 
---
+++ Udemy 2d Platformer/Assets/_Scripts/AgentAnimation.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentAnimation : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    public void PlayAnimation(AnimationType animationType)
+    {
+        switch (animationType)
+        {
+            case AnimationType.die:
+
+                break;
+            case AnimationType.hit:
+                break;
+            case AnimationType.idle:
+                Play("Idle");
+                break;
+            case AnimationType.attack:
+                break;
+            case AnimationType.run:
+                Play("Run");
+                break;
+            case AnimationType.jump:
+                break;
+            case AnimationType.fall:
+                break;
+            case AnimationType.climb:
+                break;
+            case AnimationType.land:
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void Play(string name)
+    {
+        animator.Play(name, -1, 0f);
+    }
+}
+
+public enum AnimationType
+{
+    die,
+    hit,
+    idle,
+    attack,
+    run,
+    jump,
+    fall,
+    climb,
+    land
+}
