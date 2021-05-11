1c111ce0166aaf4901812433a4ffb141796d56fc
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2021-05-11 13:31:17 +0200

    Lecture 49 Fall State Solution

---
+++ Udemy 2d Platformer/Assets/_Scripts/States/FallState.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallState : MovementState
+{
+    public float gravityModifier = 0.5f;
+
+    protected override void EnterState()
+    {
+        agent.animationManager.PlayAnimation(AnimationType.fall);
+    }
+
+    protected override void HandleJumpPressed()
+    {
+        //Dont allow jumping in fall state
+    }
+
+    public override void StateUpdate()
+    {
+        movementData.currentVelocity = agent.rb2d.velocity;
+        movementData.currentVelocity.y += gravityModifier * Physics2D.gravity.y * Time.deltaTime;
+        agent.rb2d.velocity = movementData.currentVelocity;
+
+        CalculateVelocity();
+        SetPlayerVelocity();
+
+        if (agent.groundDetector.isGrounded)
+        {
+            agent.TransitionToState(IdleState);
+        }
+    }
+
+}
