99006777597f0a9d61292ea1d50f2f757f045c87
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2021-05-11 10:52:19 +0200

    Lecture 41 Grounded Detector Gizmos

---
+++ Udemy 2d Platformer/Assets/_Scripts/GroundDetector.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDetector : MonoBehaviour
+{
+    public Collider2D agentCollider;
+    public LayerMask groundMask;
+
+    public bool isGrounded = false;
+
+    [Header("Gizmo parameters:")]
+    [Range(-2f, 2f)]
+    public float boxCastYOffset = -0.1f;
+    [Range(-2f, 2f)]
+    public float boxCastXOffset = -0.1f;
+    [Range(0, 2)]
+    public float boxCastWidth = 1, boxCastHeight = 1;
+    public Color gizmoColorNotGrounded = Color.red, gizmoColorIsGrounded = Color.green;
+
+    private void Awake()
+    {
+        if (agentCollider == null)
+            agentCollider = GetComponent<Collider2D>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (agentCollider == null)
+            return;
+        Gizmos.color = gizmoColorNotGrounded;
+        if (isGrounded == true)
+            Gizmos.color = gizmoColorIsGrounded;
+
+        Gizmos.DrawWireCube(agentCollider.bounds.center + new Vector3(boxCastXOffset, boxCastYOffset, 0),
+            new Vector3(boxCastWidth, boxCastHeight));
+
+    }
+}
