7e428851b01937a462538464ad2a29bc58cecbd9
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2021-05-27 14:06:13 +0200

    Lecture 61 Adding platforms

--- Udemy 2d Platformer/Assets/_Scripts/GroundDetector.cs
+++ Udemy 2d Platformer/Assets/_Scripts/GroundDetector.cs
@@ -30,8 +30,11 @@ public class GroundDetector : MonoBehaviour
 
         if(raycastHit.collider != null)
         {
-
-            isGrounded = true;
+            if(raycastHit.collider.IsTouching(agentCollider) == true)
+            {
+                isGrounded = true;
+            }
+            
         }
         else
         {
