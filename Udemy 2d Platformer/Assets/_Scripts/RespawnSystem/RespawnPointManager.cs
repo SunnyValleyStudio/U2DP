a9f653f9ce5bfc4873fd22cdb83a52db07e3b135
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2021-06-10 12:50:39 +0200

    Lecture 87 - Respawn Point Manager

---
+++ Udemy 2d Platformer/Assets/_Scripts/RespawnSystem/RespawnPointManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RespawnSystem
+{
+    public class RespawnPointManager : MonoBehaviour
+    {
+        List<RespawnPoint> respawnPoints = new List<RespawnPoint>();
+        RespawnPoint currentRespawnPoint;
+
+        private void Awake()
+        {
+            foreach (Transform item in transform)
+            {
+                respawnPoints.Add(item.GetComponent<RespawnPoint>());
+            }
+            currentRespawnPoint = respawnPoints[0];
+        }
+
+        public void UpdateRespawnPoint(RespawnPoint newRespawnPoint)
+        {
+            currentRespawnPoint.DisableRespawnPoint();
+            currentRespawnPoint = newRespawnPoint;
+        }
+
+        public void Respawn(GameObject objectToRespawn)
+        {
+            currentRespawnPoint.RespawnPlayer();
+            objectToRespawn.SetActive(true);
+        }
+
+        public void RespawnAt(RespawnPoint spawnPoint, GameObject playeGO)
+        {
+
+            spawnPoint.SetPlayerGO(playeGO);
+            Respawn(playeGO);
+        }
+
+        public void ResetAllSpawnPoints()
+        {
+            foreach (var item in respawnPoints)
+            {
+                item.ResetRespawnPoint();
+            }
+            currentRespawnPoint = respawnPoints[0];
+        }
+    }
+}
+
