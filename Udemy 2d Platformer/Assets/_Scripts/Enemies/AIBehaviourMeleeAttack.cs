12954b98d77f84d85c43d1d82678527d4339a504
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2021-07-06 22:41:52 +0200

    Lecture 143 - AttackBehaviour

---
+++ Udemy 2d Platformer/Assets/_Scripts/Enemies/AIBehaviourMeleeAttack.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SVS.AI
+{
+    public class AIBehaviourMeleeAttack : AIBehaviour
+    {
+        public AIMeleeAttackDetector meleeRangeDetector;
+
+        [SerializeField]
+        private bool isWaiting;
+
+        [SerializeField]
+        private float delay = 1;
+
+        private void Awake()
+        {
+            if (meleeRangeDetector == null)
+                meleeRangeDetector = GetComponentInChildren<AIMeleeAttackDetector>();
+        }
+
+        public override void PerformAction(AIEnemy enemyAI)
+        {
+            if (isWaiting)
+            {
+                return;
+            }
+            if (meleeRangeDetector.PlayerDetected == false)
+                return;
+            enemyAI.CallAttack();
+            isWaiting = true;
+            StartCoroutine(AttackDelayCoroutine());
+        }
+
+        IEnumerator AttackDelayCoroutine()
+        {
+            yield return new WaitForSeconds(delay);
+            isWaiting = false;
+
+        }
+    }
+}
