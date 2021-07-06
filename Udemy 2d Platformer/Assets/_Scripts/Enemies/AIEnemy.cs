c9613ce85bb4b55e978a9d71e7ce82d3e6983824
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2021-07-06 15:05:30 +0200

    Lecture 149 - AIEnemy

---
+++ Udemy 2d Platformer/Assets/_Scripts/Enemies/AIEnemy.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SVS.AI
+{
+    public abstract class AIEnemy : MonoBehaviour, IAgentInput
+    {
+        public Vector2 MovementVector { get; protected set; }
+
+        public event Action OnAttack;
+        public event Action OnJumpPressed;
+        public event Action OnJumpReleased;
+        public event Action<Vector2> OnMovement;
+        public event Action OnWeaponChange;
+
+        public void CallOnAttack()
+        {
+            OnAttack?.Invoke();
+        }
+
+        public void CallOnJumpPressed()
+        {
+            OnJumpPressed?.Invoke();
+        }
+
+        public void CallOnJumpReleased()
+        {
+            OnJumpReleased?.Invoke();
+        }
+
+        public void CallOnMovement(Vector2 input)
+        {
+            OnMovement?.Invoke(input);
+        }
+
+        public void CallAttack()
+        {
+            OnAttack?.Invoke();
+        }
+
+        public void CallOnWeaponChange()
+        {
+            OnWeaponChange?.Invoke();
+        }
+    }
+}
