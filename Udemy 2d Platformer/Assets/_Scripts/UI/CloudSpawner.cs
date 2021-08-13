3bd2dd02bf2b12538957b42c74e48d98d3573229
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2021-08-13 11:26:10 +0200

    Lecture 191 - Clouds spawner fixed version

--- Udemy 2d Platformer/Assets/_Scripts/UI/CloudSpawner.cs
+++ Udemy 2d Platformer/Assets/_Scripts/UI/CloudSpawner.cs
@@ -1,48 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace SVS.UI
 {
     public class CloudSpawner : MonoBehaviour
     {
         [SerializeField]
-        private float width = 10, length = 10;
+        private float width = 1000, length = 1000;
         [SerializeField]
         private Color gizmoColor = new Color(1, 0, 0, 0.2f);
         [SerializeField]
         private bool showGizmo = true;
         [SerializeField]
         private List<Cloud> cloudPrefabs = new List<Cloud>();
-        private RectTransform myRectTransform;
-        public RectTransform fronCloudParent;
 
-        private void Awake()
-        {
-            myRectTransform = GetComponent<RectTransform>();
-        }
+        [SerializeField]
+        private float cloudSpeed = 50;
+        [SerializeField]
+        private float scaleModifier = 0.5f;
+
+        public Canvas canvas;
+
 
         private void Start()
         {
-            foreach (Transform item in myRectTransform)
+            foreach (Transform item in transform)
             {
-                item.GetComponent<Cloud>().Initialize(width / 2 + 50, SpawnClouds);
-            }
-            foreach (Transform item in fronCloudParent)
-            {
-                item.GetComponent<Cloud>().Initialize(width / 2 + 50, SpawnClouds);
+                item.GetComponent<Cloud>().Initialize((width / 2) * canvas.scaleFactor + 50, SpawnClouds);
             }
         }
 
-        public void SpawnClouds()
+        private void SpawnClouds()
         {
             Vector3 position = GetRandomPosition();
-            Transform parent = myRectTransform;
 
             int cloudIndex = Random.Range(0, cloudPrefabs.Count);
             Cloud cloud = cloudPrefabs[cloudIndex];
 
-            float scale = cloud.GetCloudScale();
+            float scale = cloud.GetCloudScale() + scaleModifier;
 
             GameObject cloudObject = Instantiate(cloud.gameObject);
 
@@ -50,44 +48,35 @@ namespace SVS.UI
 
             rectTransform.position = position;
 
-            
+            rectTransform.localScale = Vector3.one * scale * canvas.scaleFactor;
 
             Cloud newCloud = cloudObject.GetComponent<Cloud>();
 
-            newCloud.speed = 50;
-            if (Random.value > 0.5)
-            {
-                parent = fronCloudParent;
-                newCloud.speed = 70;
-                scale += 0.5f;
-            }
+            newCloud.speed = cloudSpeed;
 
-            rectTransform.SetParent(parent);
+            rectTransform.SetParent(transform);
 
-            rectTransform.localScale = Vector3.one * scale;
-
-            newCloud.Initialize(width / 2 + 50, SpawnClouds);
-
+            newCloud.Initialize(width / 2 * canvas.scaleFactor + 50, SpawnClouds);
         }
 
         private Vector3 GetRandomPosition()
         {
             return new Vector3(
-                myRectTransform.position.x - width / 2 + Random.Range(0, 50), 
-                Random.Range(myRectTransform.transform.position.y - length / 2, myRectTransform.transform.position.y + length / 2), 
+                transform.position.x - (width / 2) * canvas.scaleFactor,
+                Random.Range(transform.position.y - length / 2 * canvas.scaleFactor, transform.position.y + length / 2 * canvas.scaleFactor),
                 1);
         }
 
         private void OnDrawGizmos()
         {
-            if (showGizmo)
+            if (showGizmo && canvas != null)
             {
                 Gizmos.color = gizmoColor;
-                RectTransform rectTransform = GetComponent<RectTransform>();
-                Gizmos.DrawCube(rectTransform.position, new Vector2(width, length));
+                Gizmos.DrawCube(
+                    transform.position, 
+                    new Vector2(width, length) * canvas.scaleFactor);
 
             }
-
         }
     }
 }
