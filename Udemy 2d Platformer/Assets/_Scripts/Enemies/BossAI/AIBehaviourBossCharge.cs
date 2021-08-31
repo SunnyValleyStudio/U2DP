5a01f5010511238b9cf92b47376b3d3675a18a55
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2021-08-31 15:50:23 +0200

    Lecture 245 Chase Behaviour

--- Udemy 2d Platformer/Assets/_Scripts/Enemies/BossAI/AIBehaviourBossCharge.cs
+++ Udemy 2d Platformer/Assets/_Scripts/Enemies/BossAI/AIBehaviourBossCharge.cs
@@ -1,18 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIBehaviourBossCharge : MonoBehaviour
+namespace SVS.AI
 {
-    // Start is called before the first frame update
-    void Start()
+    public class AIBehaviourBossCharge : AIBehaviour
     {
-        
-    }
+        [SerializeField]
+        private AIDataBoard aiBoard;
+        [SerializeField]
+        private AiPlayerEnterAreaDetector playerDetector;
+        [SerializeField]
+        private Agent agent;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private Vector3 tempPosition;
+        private Vector2 direction;
+
+        private bool initialized = false;
+
+        public override void PerformAction(AIEnemy enemyAI)
+        {
+            if (aiBoard.CheckBoard(AIDataTypes.Arrived))
+                initialized = false;
+
+            SetChargeDestination();
+
+            ChargeAtThePlayer(enemyAI);
+
+            if (aiBoard.CheckBoard(AIDataTypes.PathBlocked))
+            {
+                enemyAI.CallOnMovement(Vector2.zero);
+                enemyAI.MovementVector = Vector2.zero;
+                aiBoard.SetBoard(AIDataTypes.Waiting, true);
+                aiBoard.SetBoard(AIDataTypes.Arrived, true);
+            }
+        }
+
+        private void ChargeAtThePlayer(AIEnemy enemyAI)
+        {
+            enemyAI.CallOnMovement(direction.normalized);
+            enemyAI.MovementVector = direction.normalized;
+        }
+
+        private void SetChargeDestination()
+        {
+            if (initialized)
+                return;
+
+            tempPosition = new Vector2(playerDetector.Player.position.x, agent.transform.position.y);
+            direction = tempPosition - agent.transform.position;
+            aiBoard.SetBoard(AIDataTypes.Arrived, false);
+            initialized = true;
+        }
     }
 }
