1d86b270e17a6cdc785c018e4d5f1be1503edcbd
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2021-08-31 15:18:09 +0200

    Lecture 243 Boss AI Brain

---
+++ Udemy 2d Platformer/Assets/_Scripts/Enemies/BossAI/AIBossEnemyBrain.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SVS.AI
+{
+    public class AIBossEnemyBrain : AIEnemy
+    {
+        private AIDataBoard aiBoard;
+        private AiPlayerEnterAreaDetector playerDetector;
+        private AIMeleeAttackDetector meleeRangeDetector;
+        private AIEndPlatformDetector endPlatformDetector;
+
+        private AIBehaviour IdleBehaviour, ChargeBehaviour, MeleeAttackBehaviour, WaitBehaviour;
+
+        private void Update()
+        {
+            aiBoard.SetBoard(AIDataTypes.PlayerDetected, playerDetector.PlayerInArea);
+            aiBoard.SetBoard(AIDataTypes.InMeleeRange, meleeRangeDetector.PlayerDetected);
+            aiBoard.SetBoard(AIDataTypes.PathBlocked, endPlatformDetector.PathBlocked);
+
+            if (aiBoard.CheckBoard(AIDataTypes.PlayerDetected))
+            {
+                if (aiBoard.CheckBoard(AIDataTypes.Waiting))
+                {
+                    WaitBehaviour.PerformAction(this);
+                }
+                else
+                {
+                    if (aiBoard.CheckBoard(AIDataTypes.InMeleeRange))
+                    {
+                        MeleeAttackBehaviour.PerformAction(this);
+                    }
+                    else
+                    {
+                        ChargeBehaviour.PerformAction(this);
+                    }
+                }
+            }
+            else
+            {
+                IdleBehaviour.PerformAction(this);
+            }
+        }
+    }
+}
